a20e6b6009b1b4269d2689430df27abb03fdb8fd
Arsplay <ok388615@gmail.com>
2025-05-10 12:57:30 +0300

    refactor: update dispact mode for tiger

--- main.py
+++ main.py
@@ -14,10 +14,20 @@ class Tiger:
         self.y = max(0, min(self.y, 4))
         print(self.x, self.y)
 
-    def dispatch_mode(self):
+    def dispatch_mode(self, list_rabbits):
         if self.state == "hunting":
-            print("Тигр охотиться!")
+            print("Тигр охотится!")
             self.move_randomly()
+
+            if any(self.is_near_rabbit(rabbit) for rabbit in list_rabbits):
+                self.state = "attack"
+
+        if self.state == "attack":
+            print("Тигр перешёл в состояние атаки!")
+
+
+    def is_near_rabbit(self, rabbit):
+        return abs(self.x - rabbit.x) <= 1 and abs(self.y - rabbit.y) <= 1
 
 class Rabbit:
     def __init__(self, x, y):
@@ -62,13 +72,15 @@ class Game:
         self.tiger = Tiger()
         self.rabbit_first = Rabbit(random.randint(1, 4), random.randint(1, 4))
         self.rabbit_second = Rabbit(random.randint(1, 4), random.randint(1, 4))
-        self.field = Field(self.tiger, [self.rabbit_first, self.rabbit_second])
+        self.list_rabbits = [self.rabbit_first, self.rabbit_second]
+        self.field = Field(self.tiger, self.list_rabbits)
         self.game_loop()
 
     def game_loop(self):
         while self.tiger.state != "Go home":
-            self.tiger.move_randomly()
+            self.tiger.dispatch_mode(self.list_rabbits)
             self.field.update_display()
             self.field.display()
+
 
 Game()
